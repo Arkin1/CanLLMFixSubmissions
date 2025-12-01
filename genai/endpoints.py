7c58e11c2a2b5da8263e624c29013a03416ca4d5
Alexandru Stefan Stoica <stoicaastefan@gmail.com>
2025-12-01 13:16:11 +0200

    Update

--- genai/endpoints.py
+++ genai/endpoints.py
@@ -1,5 +1,6 @@
 
 import requests
+from openai import OpenAI
 
 class OllamaAPI():
     def __init__(self, endpoint:str, token:str):
@@ -17,3 +18,18 @@ class OllamaAPI():
         response.raise_for_status()
 
         return response.json()
+    
+class OpenAIAPI():
+    def __init__(self, endpoint:str, token:str):
+        self.endpoint = endpoint
+        self.token = token
+
+        self.client = OpenAI(api_key = token)
+
+    def generate(self, model:str, prompt:str):
+        response = self.client.responses.create(
+                    model=model,
+                    input=prompt
+                )
+
+        return response.output_text
--- utils.py
+++ utils.py
@@ -7,8 +7,22 @@ async def _fetch(session, url, data, headers: Optional[Dict[str, str]] = None):
         return await response.json()
 
 
-async def _compile_and_test_async(source_code:str, problem_data:dict, test_cases:list[dict], endpoint:str):
-    extension, piston_language = "cpp", "cf_c++17"
+async def _compile_and_test_async(source_code:str, problem_data:dict, test_cases:list[dict], endpoint:str, programmingLanguage: str):
+    if programmingLanguage == 'GNU C' or programmingLanguage == 'GNU C++' or programmingLanguage == 'MS C++':
+        extension, piston_language = "cpp", "cf_c++"
+    elif programmingLanguage == 'GNU C++0x':
+        extension, piston_language = "cpp", "cf_c++0x"
+    elif programmingLanguage == 'GNU C++11':
+        extension, piston_language = "cpp", "cf_c++11"
+    elif programmingLanguage == 'C++14 (GCC 6-32)':
+        extension, piston_language = "cpp", "cf_c++14"
+    elif programmingLanguage == 'C++17 (GCC 7-32)' or programmingLanguage == 'C++17 (GCC 9-64)' or programmingLanguage == 'Clang++17 Diagnostics' or programmingLanguage == 'GNU C++17 Diagnostics' or programmingLanguage == 'MS C++ 2017':
+        extension, piston_language = "cpp", "cf_c++17"
+    elif programmingLanguage == 'C++20 (GCC 11-64)' or programmingLanguage == 'C++20 (GCC 13-64)' or programmingLanguage == 'Clang++20 Diagnostics':
+        extension, piston_language = "cpp", "cf_c++20"
+    else:
+        print(f"Unsupported language {programmingLanguage}!")
+        raise Exception(f"Unsupported language {programmingLanguage}!")
     results = []
     async with aiohttp.ClientSession() as session:
         for test_case in test_cases:
@@ -42,13 +56,12 @@ async def _compile_and_test_async(source_code:str, problem_data:dict, test_cases
                 ]
             }
             
-            result = _fetch(session, f"{endpoint}/api/v2/execute", json=payload, headers={"Content-Type": "application/json"})
+            result = _fetch(session, f"{endpoint}/api/v2/execute", data=payload, headers={"Content-Type": "application/json"})
             results.append(result)
 
         results = await asyncio.gather(*results, return_exceptions=True)
 
         return results
     
-def compile_and_test(source_code:str, problem_data:dict, test_cases:list[dict], endpoint:str):
-    return asyncio.run(_compile_and_test_async(source_code, problem_data, test_cases, endpoint))
-
+async def compile_and_test(source_code:str, problem_data:dict, test_cases:list[dict], endpoint:str, programmingLanguage:str):
+    return await _compile_and_test_async(source_code, problem_data, test_cases, endpoint, programmingLanguage)
